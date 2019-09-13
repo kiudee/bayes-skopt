841862b4f674442a17cfdf0d125414ac69953434
Karlson Pfannschmidt <kiudee@mail.upb.de>
2019-09-13 12:30:20 +0200

    Write docstring for BayesGPR

--- bask/bayesgpr.py
+++ bask/bayesgpr.py
@@ -14,6 +14,118 @@ __all__ = ["BayesGPR"]
 
 
 class BayesGPR(GaussianProcessRegressor):
+    """ Gaussian process regressor of which the kernel hyperparameters are inferred in a fully Bayesian framework.
+
+    The implementation is based on Algorithm 2.1 of Gaussian Processes for Machine Learning (GPML) by Rasmussen and
+    Williams.
+
+    In addition to standard scikit-learn estimator API, GaussianProcessRegressor:
+       * allows prediction without prior fitting (based on the GP prior);
+       * provides an additional method sample_y(X), which evaluates samples drawn from the GPR (prior or posterior or
+         hyper-posterior) at given inputs;
+       * exposes a method log_marginal_likelihood(theta), which can be used externally for other ways of selecting
+         hyperparameters, e.g., via Markov chain Monte Carlo.
+       * allows setting the kernel hyperparameters while correctly recalculating the required matrices
+       * exposes a method noise_set_to_zero() which can be used as a context manager to temporarily set the prediction
+         noise to zero. This is useful for evaluating acquisition functions for Bayesian optimization
+
+    Parameters
+    ----------
+    kernel : kernel object
+        The kernel specifying the covariance function of the GP. If None is
+        passed, the kernel "1.0 * RBF(1.0)" is used as default. Note that
+        the kernel's hyperparameters are set to the geometric median of the
+        Markov chain Monte Carlo samples of the posterior.
+    alpha : float or array-like, optional (default: 1e-10)
+        Value added to the diagonal of the kernel matrix during fitting.
+        Larger values correspond to increased noise level in the observations.
+        This can also prevent a potential numerical issue during fitting, by
+        ensuring that the calculated values form a positive definite matrix.
+        If an array is passed, it must have the same number of entries as the
+        data used for fitting and is used as datapoint-dependent noise level.
+        Note that this is equivalent to adding a WhiteKernel with c=alpha.
+        Allowing to specify the noise level directly as a parameter is mainly
+        for convenience and for consistency with Ridge.
+        Also note, that this class adds a WhiteKernel automatically if noise
+        is set.
+    optimizer : string or callable, optional (default: "fmin_l_bfgs_b")
+        Can either be one of the internally supported optimizers for optimizing
+        the kernel's parameters, specified by a string, or an externally
+        defined optimizer passed as a callable. If a callable is passed, it
+        must have the signature::
+            def optimizer(obj_func, initial_theta, bounds):
+                # * 'obj_func' is the objective function to be minimized, which
+                #   takes the hyperparameters theta as parameter and an
+                #   optional flag eval_gradient, which determines if the
+                #   gradient is returned additionally to the function value
+                # * 'initial_theta': the initial value for theta, which can be
+                #   used by local optimizers
+                # * 'bounds': the bounds on the values of theta
+                ....
+                # Returned are the best found hyperparameters theta and
+                # the corresponding value of the target function.
+                return theta_opt, func_min
+        Per default, the 'fmin_l_bfgs_b' algorithm from scipy.optimize
+        is used. If None is passed, the kernel's parameters are kept fixed.
+        Available internal optimizers are::
+            'fmin_l_bfgs_b'
+        Note, that the kernel hyperparameters obtained are only used as the
+        initial position of the Markov chain and will be discarded afterwards.
+    n_restarts_optimizer : int, optional (default: 0)
+        The number of restarts of the optimizer for finding the kernel's
+        parameters which maximize the log-marginal likelihood. The first run
+        of the optimizer is performed from the kernel's initial parameters,
+        the remaining ones (if any) from thetas sampled log-uniform randomly
+        from the space of allowed theta-values. If greater than 0, all bounds
+        must be finite. Note that n_restarts_optimizer == 0 implies that one
+        run is performed.
+    normalize_y : boolean, optional (default: False)
+        Whether the target values y are normalized, i.e., the mean of the
+        observed target values become zero. This parameter should be set to
+        True if the target values' mean is expected to differ considerable from
+        zero. When enabled, the normalization effectively modifies the GP's
+        prior based on the data, which contradicts the likelihood principle;
+        normalization is thus disabled per default.
+    copy_X_train : bool, optional (default: True)
+        If True, a persistent copy of the training data is stored in the
+        object. Otherwise, just a reference to the training data is stored,
+        which might cause predictions to change if the data is modified
+        externally.
+    random_state : int, RandomState instance or None, optional (default: None)
+        The generator used to initialize the centers. If int, random_state is
+        the seed used by the random number generator; If RandomState instance,
+        random_state is the random number generator; If None, the random number
+        generator is the RandomState instance used by `np.random`.
+    noise :  string, optional (default: "gaussian")
+        If set to "gaussian", then it is assumed that `y` is a noisy
+        estimate of `f(x)` where the noise is gaussian.
+        A WhiteKernel will be added to the provided kernel.
+
+    Attributes
+    ----------
+    X_train_ : array-like, shape = (n_samples, n_features)
+        Feature values in training data (also required for prediction)
+    y_train_ : array-like, shape = (n_samples, [n_output_dims])
+        Target values in training data (also required for prediction)
+    kernel_ : kernel object
+        The kernel used for prediction. The structure of the kernel is the
+        same as the one passed as parameter but with optimized hyperparameters
+    L_ : array-like, shape = (n_samples, n_samples)
+        Lower-triangular Cholesky decomposition of the kernel in ``X_train_``
+    alpha_ : array-like, shape = (n_samples,)
+        Dual coefficients of training data points in kernel space
+    log_marginal_likelihood_value_ : float
+        The log-marginal-likelihood of ``self.kernel_.theta``
+    noise_ : float
+        Estimate of the gaussian noise. Useful only when noise is set to
+        "gaussian".
+    chain_ : array-like, shape = (n_desired_samples, n_hyperparameters)
+        Samples from the posterior distribution of the hyperparameters.
+    pos_ : array-like, shape = (n_walkers, n_hyperparameters)
+        Last position of the Markov chain. Useful for continuing sampling when new datapoints arrive.
+        fit(X, y) internally uses an existing pos_ to resume sampling, if no other position is provided.
+    """
+
     def __init__(
         self,
         kernel=None,

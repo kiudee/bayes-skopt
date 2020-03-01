cc03f0f7bd579109f5b32e5cb813d46597dcf400
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-03-01 15:07:01 +0100

    Add support for noisy points in optimizer

--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -20,11 +20,30 @@ def test_multiple_asks():
 def test_initial_points():
     opt = Optimizer(dimensions=[(-2.0, 2.0)], n_initial_points=5)
     x = opt.ask()
-    opt.tell([x], [0.])
+    opt.tell([x], [0.0])
     assert opt._n_initial_points == opt.n_initial_points_ - 1
 
-    opt.tell([x], [0.])
+    opt.tell([x], [0.0])
     assert opt._n_initial_points == opt.n_initial_points_ - 2
 
     opt.tell([[0.1], [0.2], [0.3]], [0.0, 0.1, 0.2], replace=True)
     assert opt._n_initial_points == opt.n_initial_points_ - 3
+
+
+def test_noise_vector():
+    opt = Optimizer(dimensions=[(-2.0, 2.0)], n_initial_points=5)
+    opt.tell(
+        [[-1.0], [0.0], [1.0], [0.5]],
+        [0.0, -1.0, 0.0, -1.0],
+        noise_vector=[1.0, 1.0, 1.0, 0.0],
+    )
+    x = opt.ask()
+    opt.tell([x], [0.0])
+    # Test, if the less noisy optimum (at 0.5) had a stronger impact on the mean process
+    # than the noisy optimum (at 0.0):
+    y_noisy, y = opt.gp.predict([[0.5], [0.625]])
+    assert y_noisy > y
+
+    # Check, if passing a single point works correctly:
+    x = opt.ask()
+    opt.tell(x, 0.0, noise_vector=0.5)

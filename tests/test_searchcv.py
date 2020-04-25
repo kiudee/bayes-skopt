3a4a075e6cb5bd44d5a09982fcf9b3decd29caf0
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-04-25 14:10:58 +0200

    Add test to check if the new "best_mean" return policy works as expected.

--- tests/test_searchcv.py
+++ tests/test_searchcv.py
@@ -25,3 +25,26 @@ def test_searchcv_run():
 
     opt.fit(X_train, y_train)
     assert opt.score(X_test, y_test) > 0.9
+
+
+def test_searchcv_best_mean():
+    X, y = load_iris(True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=0.75, random_state=0
+    )
+
+    opt = BayesSearchCV(
+        SVC(),
+        {
+            "C": Real(1e-6, 1e6, prior="log-uniform"),
+            "gamma": Real(1e-6, 1e1, prior="log-uniform"),
+            "degree": Integer(1, 8),
+            "kernel": Categorical(["linear", "poly", "rbf"]),
+        },
+        n_iter=11,
+        cv=None,
+        return_policy="best_mean"
+    )
+
+    opt.fit(X_train, y_train)
+    assert opt.score(X_test, y_test) > 0.9

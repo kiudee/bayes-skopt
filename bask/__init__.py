b23961c48a708c74e9225ed9dce2f4d9f80969a2
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-05-21 14:18:36 +0200

    Bump version: 0.4.1 → 0.5.0

--- bask/__init__.py
+++ bask/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Karlson Pfannschmidt"""
 __email__ = "kiudee@mail.upb.de"
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 
 from .acquisition import *
 from .bayesgpr import BayesGPR
--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kiudee/bayes-skopt",
-    version="0.4.1",
+    version="0.5.0",
     zip_safe=False,
 )

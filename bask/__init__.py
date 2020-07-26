fc8c65c880b27ec8a29070b9151cc8336108ac86
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-07-26 13:17:06 +0200

    Bump version: 0.6.0 → 0.7.0

--- bask/__init__.py
+++ bask/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Karlson Pfannschmidt"""
 __email__ = "kiudee@mail.upb.de"
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 from .acquisition import *
 from .bayesgpr import BayesGPR

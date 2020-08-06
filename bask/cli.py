2fb38f209534443b20b2162e1fddf93aed5cedca
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-08-06 12:04:28 +0200

    Apply black to all source files

--- bask/cli.py
+++ bask/cli.py
@@ -7,8 +7,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for bask."""
-    click.echo("Replace this message by putting your code into "
-               "bask.cli.main")
+    click.echo("Replace this message by putting your code into bask.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
--- tests/test_bayesgpr.py
+++ tests/test_bayesgpr.py
@@ -8,10 +8,9 @@ from bask.bayesgpr import BayesGPR
 
 @pytest.fixture
 def minimal_gp():
-    kernel = (
-        ConstantKernel(constant_value=1 ** 2, constant_value_bounds=(0.01 ** 2, 1 ** 2))
-        * RBF(length_scale=1.0, length_scale_bounds=(0.5, 1.5))
-    )
+    kernel = ConstantKernel(
+        constant_value=1 ** 2, constant_value_bounds=(0.01 ** 2, 1 ** 2)
+    ) * RBF(length_scale=1.0, length_scale_bounds=(0.5, 1.5))
     gp = BayesGPR(random_state=1, normalize_y=False, kernel=kernel)
     return gp
 
@@ -19,9 +18,13 @@ def minimal_gp():
 @pytest.fixture
 def minimal_priors():
     return [
-        lambda x: halfnorm(scale=1.).logpdf(np.sqrt(np.exp(x))) + x / 2.0 - np.log(2.0),
-        lambda x: invgamma(a=5.0, scale=1.).logpdf(np.exp(x)) + x,
-        lambda x: halfnorm(scale=1.).logpdf(np.sqrt(np.exp(x))) + x / 2.0 - np.log(2.0)
+        lambda x: halfnorm(scale=1.0).logpdf(np.sqrt(np.exp(x)))
+        + x / 2.0
+        - np.log(2.0),
+        lambda x: invgamma(a=5.0, scale=1.0).logpdf(np.exp(x)) + x,
+        lambda x: halfnorm(scale=1.0).logpdf(np.sqrt(np.exp(x)))
+        + x / 2.0
+        - np.log(2.0),
     ]
 
 
@@ -29,9 +32,18 @@ def test_noise_vector(minimal_gp, minimal_priors):
     X = np.array([[0.0], [0.0]])
     y = np.array([1.0, 0.0])
     noise_vector = np.array([1234, 0.0])
-    minimal_gp.fit(X, y, noise_vector=noise_vector, n_burnin=1, progress=False, priors=minimal_priors)
+    minimal_gp.fit(
+        X,
+        y,
+        noise_vector=noise_vector,
+        n_burnin=1,
+        progress=False,
+        priors=minimal_priors,
+    )
     prediction = minimal_gp.predict(np.array([[0.0]]))
-    assert prediction < 0.01  # The high noise is supposed to diminish the effect of the datapoint
+    assert (
+        prediction < 0.01
+    )  # The high noise is supposed to diminish the effect of the datapoint
 
 
 def test_noise_set_to_zero(minimal_gp, minimal_priors):
@@ -49,5 +61,3 @@ def test_sample_without_fit(minimal_gp):
     # Calling sample without data (X, y) or a previous fit, should raise a ValueError:
     with pytest.raises(ValueError):
         minimal_gp.sample()
-
-

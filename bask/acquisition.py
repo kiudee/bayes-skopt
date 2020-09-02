Fix a bug in the calculation of MaxValueSearch and refactor the code to make it clearer.

bask/acquisition.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 import scipy.stats as st
 from scipy.linalg import cho_solve, cholesky
-from scipy.optimize import bisect
+from scipy.optimize import brentq
 from sklearn.utils import check_random_state
 
 from bask.utils import get_progress_bar, validate_zeroone
@@ -229,34 +229,31 @@ class MaxValueSearch(UncertaintyAcquisition):
 
     def __call__(self, mu, std, *args, n_min_samples=1000, **kwargs):
         def probf(x):
-            return np.exp(np.sum(st.norm.logcdf(-(x - mu) / std), axis=0))
+            return np.exp(np.sum(st.norm.logcdf((x - mean) / std), axis=0))
 
-        idx = np.argmin(mu)
-        right = mu[idx].flatten()
-        left = right
-        i = 0
-        while probf(left) < 0.75:
-            left = 2.0 ** i * np.min(mu - 5.0 * std) + (1.0 - 2.0 ** i) * right
-            i += 1
+        # Negative sign, since the original algorithm is defined in terms of the maximum
+        mean = -mu
+        left = np.min(mean - 3 * std)
+        right = np.max(mean + 5 * std)
         # Binary search for 3 percentiles
-        q1, med, q2 = map(
-            lambda val: bisect(
-                lambda x: probf(x) - val, left, right, maxiter=10000, xtol=0.01
-            ),
-            [0.25, 0.5, 0.75],
-        )
+        q1, med, q2 = [
+            brentq(lambda x: probf(x) - val, left, right,) for val in [0.25, 0.5, 0.75]
+        ]
         beta = (q1 - q2) / (np.log(np.log(4.0 / 3.0)) - np.log(np.log(4.0)))
         alpha = med + beta * np.log(np.log(2.0))
-        mins = (
+        max_values = (
             -np.log(-np.log(np.random.rand(n_min_samples).astype(np.float32))) * beta
             + alpha
         )
 
-        gamma = (mu[:, None] - mins[None, :]) / std[:, None]
-        return np.sum(
-            gamma * st.norm().pdf(gamma) / (2.0 * st.norm().cdf(gamma))
-            - st.norm().logcdf(gamma),
-            axis=1,
+        gamma = (max_values[None, :] - mean[:, None]) / std[:, None]
+        return (
+            np.sum(
+                gamma * st.norm().pdf(gamma) / (2.0 * st.norm().cdf(gamma))
+                - st.norm().logcdf(gamma),
+                axis=1,
+            )
+            / n_min_samples
         )
 
 